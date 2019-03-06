4be200ca92b2f3b89a07a876f0a1bd0f428f6b6d
vicroseau <victor.roseau@hotmail.fr>
2019-03-06 11:00:11 +0100

    fouthcommit

---
+++ src/app/module-formateurs/formateur.ts
@@ -0,0 +1,8 @@
+export class Formateur {
+    profession: number;
+    id: number;
+    nom: string;
+    prenom: string;
+    email: string;
+    telephone: string;
+}
--- src/app/module-formateurs/module-formateurs.component.ts
+++ src/app/module-formateurs/module-formateurs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Formateur} from './formateur';
+import {FormateurService} from './module-formateur.service'
 
 @Component({
   selector: 'app-module-formateurs',
@@ -8,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModuleFormateursComponent implements OnInit {
 
-  constructor() { }
+formateurs;
+selectedFormateur: Formateur;
+
+
+  constructor(private formateurService: FormateurService) { }
 
 
   ngOnInit() {
-  
+   
+this.getFormateurs();
+
   }
+  onSelectFormateur(formateur: Formateur): void{
+    this.selectedFormateur=formateur;
+    
+  }
+
+  
+  getFormateurs(): void{
+    this.formateurService.getFormateur()
+        .subscribe(formateur => this.formateurs = formateur);
+  }
+  
 }
